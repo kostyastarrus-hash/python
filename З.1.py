c29c7d0be8c3eacca2a4c7a3032af153b61cb77b
kostyastarrus-hash <kostyastar.rus@gmail.com>
2025-11-18 12:51:45 +0300

    Create З.1.py

---
+++ З.1.py
@@ -0,0 +1,18 @@
+first_num = 9
+second_num = 7.8
+my_str = ["start", "stop"]
+#print(first_num,second_num,my_str)
+
+
+
+first_num = 5.2
+#print (first_num,type(first_num))
+var = third_num = first_num + second_num
+#print(third_num)
+first_num += 5
+second_num += first_num
+#print(first_num,second_num)
+second_num = 10
+#print(first_num,second_num)
+my_str *= 5
+print(my_str)
